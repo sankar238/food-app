bea16b53ff692bb1c7df2655c53cb8f84db384da
sankar <ramunaidukoripilli@gmail.com>
2023-11-09 15:14:27 +0530

    introducing parcel

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM  from "react-dom/client";
+
 const heading = React.createElement(
     "h1",
     { id: "heading" },
@@ -26,11 +29,11 @@ const parent = React.createElement(
     "div",
     { id: "parent" },
     [React.createElement(
-        "div",
+        "div",  
         { id: "child1" },
         [React.createElement(
             "h1", {},
-            "i am h1 tag"
+            "this is h1 tag 🚀"
         ), React.createElement(
             "h2", {},
             "i am h2 tag"
@@ -47,5 +50,6 @@ const parent = React.createElement(
         )]
     )]
 )
-console.log(parent)
+// console.log(parent)
+
 root.render(parent)
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Namaste React food app
--creating Hello World using React by CDN links.
+
+
+# Parcel 
+ - Dev Build
+ - cretes local server
+ - HMR (hot module replcement)
+ - File watching algorithm -written in c++
+ - caching - faster builds
+ - Image Optimization
+ - minification
+ - bundling
+ - compressing
+ - consistent hashing
+ - code splitting
+ - differential bundling - to support older browsers
+ - diagnostics - better error suggestions
+ - Tree Shaking - removes unused code for us.
+ - different builds for dev phase and production phase   
